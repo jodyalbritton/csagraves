15520c1636a38d295f50c343dcee76667e45d4e2
Jody Albritton <jody.albritton@gmail.com>
2014-05-19 00:10:29 -0500

    fix geocoder

--- app/models/cemetery.rb
+++ app/models/cemetery.rb
@@ -8,9 +8,26 @@ class Cemetery < ActiveRecord::Base
 	
 	has_attached_file :logo, :styles => { :medium => "300x300>", :thumb => "50x50>" }, :default_url =>":style/missing.png"
     validates_attachment_content_type :logo, :content_type => /\Aimage\/.*\Z/
-    after_validation :geocode          # auto-fetch coordinates
+    after_validation 
+            if :latitude != nil 
+                :reverse_geocode
+            else 
+                :geocode 
+            end
+
 
     geocoded_by :full_street_address   # can also be an IP address
+
+    reverse_geocoded_by :latitude, :longitude do |obj,results|
+      if geo = results.first
+        foo = geo.address.split(',')
+        obj.address = foo[0]
+        obj.city    = geo.city
+        obj.zip = geo.postal_code
+        obj.state = geo.state
+      end
+end
+after_validation :reverse_geocode
     
     def full_street_address 
     	[address, address_two, city, state, zip].compact.join(', ')
